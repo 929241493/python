3d82b2e1bb1fba09f6889802c326bedc8b21c388
929241493 <929241493@qq.com>
2016-05-19 19:57:43 +0800

    开时
    跑出链接来

--- 1.py
+++ 1.py
@@ -1,3 +1,12 @@
-import urllib2
-response = urllib2.urlopen("http://www.baidu.com")
-print response.read()
+# coding:utf-8
+import re
+import requests
+# 获取网页内容
+r = requests.get('http://www.njupt.edu.cn/')
+data = r.text
+# 利用正则查找所有连接
+link_list =re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')" ,data)
+for url in link_list:
+    print (url)
+    
+#尽管看不懂代码但是跑出来了
